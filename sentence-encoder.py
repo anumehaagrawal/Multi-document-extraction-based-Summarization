2946f72142d96d539c2329ce11ed651d252246d6
Sunitha Selvan <sunitha98selvan@gmail.com>
2019-03-07 14:13:20 +0530

    stack decoder

--- sentence-encoder.py
+++ sentence-encoder.py
@@ -1,12 +1,22 @@
 import sentance_imp1 as fv
-		
-maxLen = 100
-decoder_stacks = [None for i in range(maxLen)]
+from sklearn.metrics.pairwise import cosine_similarity
+
+maxLen = 101
+decoder_stacks = [[ ] for i in range(maxLen)]
 sentences_array=[]
+
+def cosine_similarity(sentences,sentence_vectors):
+	sim_mat = np.zeros([len(sentences), len(sentences)])
+	for i in range(len(sentences)):
+		for j in range(len(sentences)):
+			if i != j:
+				sim_mat[i][j] = cosine_similarity(sentence_vectors[i].reshape(1,100), sentence_vectors[j].reshape(1,100))[0,0]
+	return sim_mat
 
 def create_sentence_val():
     doc_array = fv.get_documents()
     doc_order = fv.get_doc_order()
+
     print(doc_order)
     
     for doc in range(10):
@@ -15,15 +25,37 @@ def create_sentence_val():
             sentences = doc_array[doc][0].split(".")
             #doc_num = doc_order[doc]
             for group in sentences:
-                print(group)
                 feature_vec = fv.tf_idf_sentence(group,doc)
                 mapping = [group,sum(feature_vec)]
                 sentences_array.append(mapping)
 
+def importance(sentence_stack):
+    sum =0
+    for i in sentence_stack:
+        sum+=sentance_stack[1]
+    return sum
+
 def stack_decoder():
-    for num in range(maxLen):
-        for j in range(num):
-            decoder_stacks[num].append(0)
+    threshold=0.5
+    stack=[] #priority queue
+    for i in range(maxLen):
+        for j in decoder_stacks:
+            for s in sentences_array:
+                newLen=maxLen+1
+                #decoder_stacks[num].append(0)
+                if i+ len(s)<maxLen:
+                    newLen=i+len(s[0])
+                    if len(j)==0:
+                        j.append(s)
+                    elif cosine_similarity(s[0],j[0]) < threshold:
+                        j.append(s)
+                    score=importance(j)
+                    decoder_stacks[newLen].append([j,score])
+    print(decoder_stacks)
+
+                    
+                    
+
 
 create_sentence_val()
 print(sentences_array)
