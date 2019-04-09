42afb799b4d2be7947a11cf3851193e6b0c5b750
anumehaagrawal <anuzenith29@gmail.com>
2019-04-10 01:53:20 +0530

    Summary

--- PyRouge/pyrouge.py
+++ PyRouge/pyrouge.py
@@ -1,6 +1,6 @@
 from __future__ import division
 from itertools import chain
-
+import os
 
 def get_unigram_count(tokens):
     count_dict = dict()
@@ -111,13 +111,28 @@ class Rouge:
 
 
 if __name__ == '__main__':
-    r = Rouge()
-    # A simple eample of how rouge can be calculated
-    print(r.rouge_l([[1, 7, 6, 7, 5], [0, 2, 8, 3, 5]],
-                    [[1, 2, 3, 4, 5], [3, 9, 5]]))
-
-    # A more practical example of how it can be used for summary evaluation
-    system_generated_summary = " The Kyrgyz President pushed through the law requiring the use of ink during the upcoming Parliamentary and Presidential elections In an effort to live up to its reputation in the 1990s as an island of democracy. The use of ink is one part of a general effort to show commitment towards more open elections. improper use of this type of ink can cause additional problems as the elections in Afghanistan showed. The use of ink and readers by itself is not a panacea for election ills."
-    manual_summmary = " The use of invisible ink and ultraviolet readers in the elections of the Kyrgyz Republic which is a small, mountainous state of the former Soviet republic, causing both worries and guarded optimism among different sectors of the population. Though the actual technology behind the ink is not complicated, the presence of ultraviolet light (of the kind used to verify money) causes the ink to glow with a neon yellow light. But, this use of the new technology has caused a lot of problems. "
-
-    print(r.rouge_l([system_generated_summary], [manual_summmary]))
+    file=open("rouge_scores.txt","a")
+    root_dir = '/home/sunitha/Desktop/6th_sem/IR/Multi-document-extraction-based-Summarization/summaries/page_rank/'
+    dataset_dir= '/home/sunitha/Desktop/6th_sem/IR/Multi-document-extraction-based-Summarization/dataset/Test_Summaries/'
+    for i in ["1","17","20","22","24","27","29","31","36","38","44"]:
+        file1= root_dir+i+".py"
+        if len(i)==1:
+            file2= dataset_dir +"d3000"+ i +"t/D3000"+i+".M.100.T.D"
+        else:
+             file2= dataset_dir +"d300"+ i +"t/D300"+i+".M.100.T.B"
+        file.write(file1)
+        file.write("\n")
+        with open(file1) as f:
+            para = f.readlines()
+            para=para[0]
+            text=para[10:len(para)-2]
+            #print(text)
+            r = Rouge()                
+            system_generated_summary = text
+        with open(file2) as f:
+            para=f.readlines()
+            #print(para)
+            text= " ".join(para)
+            manual_summmary = text
+        file.write(str(r.rouge_l([system_generated_summary], [manual_summmary])))
+file.write("\n")
